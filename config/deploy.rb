9e33e361ce96481b8a469076e498af653e0f6960
Duy Ho <duyht@lifull-tech.vn>
2018-09-10 14:53:44 +0700

    update cap files

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+repo_url = 'git@github.com:duyhotung/RoR.git'
+application = 'ror'
+#local_secret_key_path = 'config/secrets.yml.key'
+
+set :application, application
+set :repo_url, repo_url
+append :linked_dirs, 'log', 'tmp/sockets', 'tmp/pids', 'tmp/cache', 'vendor/bundle', 'public/system'
+append :linked_files, local_secret_key_path
+#append :linked_files, 'config/master.key'
+
+set :default_env, path: '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
+set :keep_releases, 5
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :deploy_to, '/var/www'
+set :log_level, :debug
+set :preload_app, true
+
+namespace :deploy do
+  task upload_secret_key: [:set_rails_env] do
+    on roles(:web) do
+      execute :mkdir, '-p', "#{shared_path}/config"
+      if File.exist? local_secret_key_path
+        upload! local_secret_key_path, "#{shared_path}/config/secrets.yml.key"
+      end
+    end
+  end
+  before 'deploy:check:linked_files', 'deploy:upload_secret_key'
+
+  desc 'ridgepole apply'
+  task ridgepole_apply: [:set_rails_env] do
+    on primary(:db) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'ridgepole:apply'
+        end
+      end
+    end
+  end
+  after 'deploy:updated', 'deploy:ridgepole_apply'
+
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+  after 'deploy:publishing', 'deploy:restart'
+end
+
---
+++ config/deploy/staging.rb
@@ -0,0 +1,11 @@
+set :stage, :staging
+set :rails_env, 'staging'
+set :branch, 'develop'
+set :bundle_without, %i[test]
+
+set :sidekiq_options_per_process, ['-q default -q cron -q mailers']
+set :sidekiq_processes, 1
+set :sidekiq_role, %i[worker]
+
+server '13.231.95.80', user: 'ec2-user', roles: %w[web app worker]
+
