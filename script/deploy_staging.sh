d4b15585039015394d712d3b4d667ab9b1d9c040
Duy Ho <duyht@lifull-tech.vn>
2018-12-04 09:45:35 +0700

    testIP

--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -9,4 +9,5 @@ MYIP=`curl -f -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
 bundle exec cap staging deploy
+echo "Removing $MYIP from security group:$MYSECURITYGROUP"
 aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
